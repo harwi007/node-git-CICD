732d0cf02267fb73133e40384d972b078c5398fb
harw <harwinder.codebrew@gmail.com>
2022-06-01 10:26:58 +0530

    Change the message fro whole world

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('Hello to you from a Very junior DevOps!')
+  res.send('The Living Legend- One & only  Sidhu Moosewala!')
 })
 
 app.get('/status', (req, res) => {
